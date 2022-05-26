cdf1c4d9391777cbb409345ea1d5b6df3d69e53b
ArtMoc <tem.mochalov@yandex.ru>
2022-05-26 13:30:59 +0300

    Add project files.

---
+++ Calendar/ButtonCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    class ButtonCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        Action _action;
+        public ButtonCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ Calendar/ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Calendar
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        string _name;
+        long _tel;
+        DateTime _bday;
+        Person _person = new Person("", 0, DateTime.Now);
+
+
+        public ICommand OkCommand
+        {
+            get
+            {
+                return new ButtonCommand(new Action(() =>
+
+                {
+                    MessageBox.Show(_person.Name);
+                }
+            ));
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                _person = new Person(Name, Tel, Bday);
+                Notify("Name");
+            }
+        }
+        public long Tel
+        {
+            get { return _tel; }
+            set
+            {
+                _tel = value;
+                _person = new Person(Name, Tel, Bday);
+                Notify("Tel");
+            }
+        }
+        public DateTime Bday
+        {
+            get { return _bday; }
+            set
+            {
+                _bday = value;
+                _person = new Person(Name, Tel, Bday);
+                Notify("Bday");
+            }
+        }
+    }
+}
